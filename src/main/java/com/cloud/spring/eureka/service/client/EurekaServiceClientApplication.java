9e804925e7485d8a7cee625c978223591440f2a4
taohui <htao@thoughtworks.com>
2019-07-10 15:45:17 +0800

    [taohui] init empty spring boot app

---
+++ src/main/java/com/cloud/spring/eureka/service/client/EurekaServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.spring.eureka.service.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/spring/eureka/service/client/EurekaServiceClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.spring.eureka.service.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServiceClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
