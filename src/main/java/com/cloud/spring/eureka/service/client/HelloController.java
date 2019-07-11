b9a31149facb11bb7b726965be8509afc0974214
taohui <htao@thoughtworks.com>
2019-07-11 17:44:13 +0800

    [taohui] print server.port to make sure ribbon balancer work

--- src/main/java/com/cloud/spring/eureka/service/client/HelloController.java
+++ src/main/java/com/cloud/spring/eureka/service/client/HelloController.java
@@ -1,5 +1,6 @@
 package com.cloud.spring.eureka.service.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    String port;
+
     @GetMapping("/hello")
     public String index(){
-        return "hello world";
+        return "hello world from port:" + port;
     }
 }
