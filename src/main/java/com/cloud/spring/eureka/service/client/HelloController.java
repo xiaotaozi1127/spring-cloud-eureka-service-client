2ad761b091fda2c1a02b3ff102ecd6b6112cc2d6
taohui <htao@thoughtworks.com>
2019-07-15 17:42:15 +0800

    [taohui] add a new service with request parameter

--- src/main/java/com/cloud/spring/eureka/service/client/HelloController.java
+++ src/main/java/com/cloud/spring/eureka/service/client/HelloController.java
@@ -2,6 +2,7 @@ package com.cloud.spring.eureka.service.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,5 +15,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String index(){
         return "hello world from port:" + port;
+    }
+
+    @GetMapping("/hello1")
+    public String hello(@RequestParam String name){
+        return "Hello " + name;
     }
 }
